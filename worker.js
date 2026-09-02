1030afd0f5ee7ca9d225314292e07910c30fb691
ecreator <jimboy18102gmail.com>
2026-09-02 11:01:41 +0800

    fixing rpc

--- worker.js
+++ worker.js
@@ -1,10 +1,29 @@
 export default {
   async fetch(request, env) {
     const url = new URL(request.url);
+    const pathname = url.pathname.replace(/\/+$/, "");
+    const match = pathname.match(/^\/t\/([^/]+)$/i);
 
-    if (/^\/t\/[^/]+$/i.test(url.pathname.replace(/\/+$/, ""))) {
+    if (match) {
+      const token = decodeURIComponent(match[1]);
       const customerUrl = new URL("/customer/index.html", url);
-      return env.ASSETS.fetch(new Request(customerUrl, request));
+      const assetResponse = await env.ASSETS.fetch(new Request(customerUrl, request));
+
+      if (!assetResponse.ok) return assetResponse;
+
+      const html = await assetResponse.text();
+      const tokenScript = `<script>window.__ORDELI_TRACKING_TOKEN=${JSON.stringify(token)};</script>`;
+      const patchedHtml = html.replace(/<\\/head>/i, `${tokenScript}</head>`);
+
+      const headers = new Headers(assetResponse.headers);
+      headers.set("content-type", "text/html; charset=UTF-8");
+      headers.set("cache-control", "no-store");
+
+      return new Response(patchedHtml, {
+        status: assetResponse.status,
+        statusText: assetResponse.statusText,
+        headers
+      });
     }
 
     return env.ASSETS.fetch(request);
