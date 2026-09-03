aed324490dbbc6feb6a42c9a4db440056c2987e1
app <app.01161619@gmail.com>
2026-09-03 11:17:20 +0800

    replace sw.js for offline use

--- sw.js
+++ sw.js
@@ -1,11 +1,14 @@
 /* Ordeli seller PWA service worker.
    Customer tracking pages (/t/<token>) do not use this worker. */
 
-const CACHE_VERSION = "ordeli-v2026-09-02-05";
+const CACHE_VERSION = "ordeli-v2026-09-03-01";
 const APP_SHELL = [
   "/",
   "/index.html",
-  "/manifest.webmanifest"
+  "/manifest.webmanifest",
+  "/css/style.css",
+  "/js/supabase.js",
+  "/js/app.js?v=2026-09-02-03"
 ];
 
 self.addEventListener("install", (event) => {
