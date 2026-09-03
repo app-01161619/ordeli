3a88e520d17b05093a00f00ae615db6184ea26bf
app <app.01161619@gmail.com>
2026-09-03 15:20:13 +0800

    connectivity fix 2

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 /* Ordeli seller PWA service worker.
    Customer tracking pages (/t/<token>) do not use this worker. */
 
-const CACHE_VERSION = "ordeli-v2026-09-03-04";
+const CACHE_VERSION = "ordeli-v2026-09-03-05";
 const APP_SHELL = [
   "/",
   "/index.html",
   "/manifest.webmanifest",
   "/css/style.css",
   "/js/supabase.js",
-  "/js/app.js?v=2026-09-03-01"
+  "/js/app.js?v=2026-09-03-05"
 ];
 
 self.addEventListener("install", (event) => {
